bdfb2ca76fc5d5ba6a6b2f99523ee0aa64c9c914
ccepeda <cepeda_357@hotmail.com>
2021-06-25 13:23:54 -0500

    organiacion contenido curso frontEnd1

---
+++ courses/FrontEnd/DH_C8S/js/cv.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.skill').easyPieChart({
+        scaleColor: "#ecf0f1",
+        lineWidth: 10,
+        lineCap: 'round',
+        barColor: '#3e3e3e',
+        trackColor: "#F2F4F4",
+        size: 60,
+        animate: 2200
+    });
+});
