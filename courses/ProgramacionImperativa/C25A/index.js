ffb35a7f5ec4910e3f889fc96a2dea1ccc399ebf
ccepeda <cepeda_357@hotmail.com>
2021-10-04 20:06:30 -0500

    bot discord

---
+++ courses/ProgramacionImperativa/C25A/index.js
@@ -0,0 +1,26 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = require("./config.json");
+client.login(config.BOT_TOKEN);
+
+const prefix = "!";
+
+client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    } else if (command === "sum") {
+        const numArgs = args.map(x => parseFloat(x));
+        const sum = numArgs.reduce((counter, x) => counter += x);
+        message.reply(`The sum of all the arguments you provided is ${sum}!`);
+    }
+});
+
+client.login(config.BOT_TOKEN);
