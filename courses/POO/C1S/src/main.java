e83d059998cf8e7fefbe4a23ae9b38940031f42d
ccepeda <cepeda_357@hotmail.com>
2021-10-18 19:40:59 -0500

    poo

---
+++ courses/POO/C1S/src/main.java
@@ -0,0 +1,52 @@
+public class main {
+
+    /**
+     * @param args
+     * @author Carlos Cepeda
+     */
+    public static void main(String[] args) {
+        System.out.println("=== GRUPO 4 - CS1 ===");
+
+        //Mascota 1
+        System.out.println("---------Mascota 1--------");
+        String nombrePerro = "Manchitas";
+        int edadPerro = 2;
+        String comidaPerro = "1 kilo y medio";
+        String sonidoPerro = "guau guau";
+        System.out.println(nombrePerro + " tiene " + edadPerro + " años");
+        System.out.println(nombrePerro + " come " + comidaPerro + " y hace " + sonidoPerro);
+
+        //Mascota 2
+        System.out.println("---------Mascota 2--------");
+        String nombrePez = "Nemo";
+        int edadPez = 1;
+        String comidaPez = "140 gramos";
+        String sonidoPez = "o o o o";
+        System.out.println(nombrePez + " tiene " + edadPez + " años");
+        System.out.println(nombrePez + " come " + comidaPez + " y hace " + sonidoPez);
+
+        System.out.println("---------Mascota 3--------");
+        String nombreGato = "Silvestre";
+        int edadGato = 3;
+        String comidaGato = "medio kilo";
+        String sonidoGato = "miau miau";
+        System.out.println(nombreGato + " tiene " + edadGato + " años");
+        System.out.println(nombreGato + " come " + comidaGato + " y hace " + sonidoGato);
+
+        System.out.println("---------Mascota 4--------");
+        String nombreTortuga = "Manuelita";
+        int edadTortuga = 12;
+        String comidaTortuga = "300 gramos";
+        String sonidoTortuga = "io io io";
+        System.out.println(nombreTortuga + " tiene " + edadTortuga + " años");
+        System.out.println(nombreTortuga + " come " + comidaTortuga + " y hace " + sonidoTortuga);
+
+        System.out.println("---------Mascota 5--------");
+        String nombrePajaro = "Gardel";
+        int edadPajaro = 1;
+        String comidaPajaro = "350 gramos";
+        String sonidoPajaro = "silvido";
+        System.out.println(nombrePajaro + " tiene " + edadPajaro + " años");
+        System.out.println(nombrePajaro + " come " + comidaPajaro + " y hace " + sonidoPajaro);
+    }
+}
