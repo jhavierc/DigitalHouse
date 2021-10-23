079e6585367333c52f5f326097a2e9e3305f328b
ccepeda <cepeda_357@hotmail.com>
2021-10-22 20:23:58 -0500

    cambios en impresion pantalla

--- courses/POO/C4S/src/com/company/Main.java
+++ courses/POO/C4S/src/com/company/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("Ingrese el nombre del jugador 2 :");
         String jugador2 = scanner.next();
 
-        System.out.println("El ganador es: " + jugar(jugador1, jugador2) +", fin del juego!!!");
+        System.out.println("El ganador es: " + jugar(jugador1, jugador2) + ", fin del juego!!!");
     }
 
     public static String jugar(String jugador1, String jugador2) {
@@ -21,38 +21,46 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String ganador = "";
         boolean hayGanador = false;
-        int turno=1;
+        int turno = 1;
 
         while (!hayGanador) {
-            System.out.println("===Turno"+turno+"====");
-            System.out.println("["+ jugador1.toUpperCase() +"] > ingresar número jugada (1=piedra, 2=papel, 3=tijera) :");
+            System.out.println("=========N° Ronda" + turno + "==========");
+            System.out.println("[" + jugador1.toUpperCase() + "] > ingrese su número jugada (1=piedra, 2=papel, 3=tijera) :");
             int jugada = scanner.nextInt();
-            System.out.println("["+ jugador2.toUpperCase() +"] > ingresar número jugada (1=piedra, 2=papel, 3=tijera) :");
+            System.out.println("[" + jugador2.toUpperCase() + "] > ingrese su número jugada (1=piedra, 2=papel, 3=tijera) :");
             int jugada2 = scanner.nextInt();
 
-            if (jugada != jugada2) {
-                if (jugada == 1 && jugada2 == 2) {
-                    ganador = jugador2;
+            if ((jugada < 0 || jugada > 3) || (jugada2 < 0 || jugada2 > 3)) {
+                System.out.println("Numero de juagada ingresado no válido!!!");
+                System.out.print("\033[H\033[2J");
+            } else {
+                if (jugada != jugada2) {
+                    if (jugada == 1 && jugada2 == 2) {
+                        ganador = jugador2;
 
-                } else if (jugada == 2 && jugada2 == 1) {
-                    ganador = jugador1;
+                    } else if (jugada == 2 && jugada2 == 1) {
+                        ganador = jugador1;
 
-                } else if (jugada == 3 && jugada2 == 1) {
-                    ganador = jugador2;
+                    } else if (jugada == 3 && jugada2 == 1) {
+                        ganador = jugador2;
 
-                } else if (jugada == 1 && jugada2 == 3) {
-                    ganador = jugador1;
+                    } else if (jugada == 1 && jugada2 == 3) {
+                        ganador = jugador1;
 
-                } else if (jugada == 2 && jugada2 == 3) {
-                    ganador = jugador2;
+                    } else if (jugada == 2 && jugada2 == 3) {
+                        ganador = jugador2;
+
+                    } else {
+                        ganador = jugador1;
+                    }
+                    hayGanador = true;
 
                 } else {
-                    ganador = jugador1;
+                    System.out.println("Empate!!!, siguiente ronda");
                 }
-                hayGanador = true;
-
+                turno++;
             }
-            turno++;
+
 
         }
         return ganador.toUpperCase(Locale.ROOT);
